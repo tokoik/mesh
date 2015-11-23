b7e21f2811579f8a4b93e4274c4e16add856dae1
Kohe Tokoi <tokoik@gmail.com>
2015-11-23 22:49:03 +0900

    Initial Commit

---
+++ mesh/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mesh
+//
+//  Created by こうへい on 2015/11/23.
+//  Copyright © 2015年 Kohe Tokoi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
